d1a4ea544720483363f98f4a91c32346017ff678
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-05-28 12:30:36 -0300

    Clean and update previous configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,16 @@
 import Head from 'next/head';
 import React from 'react';
-import { Container } from '../styles/pages/Home';
 
 const Home: React.FC = () => {
     return (
-        <Container>
+        <>
             <Head>
                 <title>Spotify Playlist Guard</title>
             </Head>
 
             <h1>Hello world!</h1>
             <p>A website for the Spotify Playlist Guard application.</p>
-        </Container>
+        </>
     );
 };
 
