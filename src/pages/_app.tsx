36b8fb7028e8892be93d7eb63437d2ead964621d
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-07-15 11:40:27 -0300

    feat: create QueryProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,23 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '../styles/theme';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AuthProvider } from '../contexts/AuthContext';
+import { QueryProvider } from '../contexts/QueryContext';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <AuthProvider>
-            <Head>
-                <meta
-                    name="viewport"
-                    content="initial-scale=1, width=device-width"
-                />
-            </Head>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Component {...pageProps} />
-            </ThemeProvider>
+            <QueryProvider>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1, width=device-width"
+                    />
+                </Head>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </QueryProvider>
         </AuthProvider>
     );
 };
