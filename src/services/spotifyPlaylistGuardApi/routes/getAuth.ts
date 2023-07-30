024ef1a5d76cd433ea72899fcb4687299a7bb772
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-07-30 20:23:14 -0300

    refactor: update return values from getAuth service and handle status

--- src/services/spotifyPlaylistGuardApi/routes/getAuth.ts
+++ src/services/spotifyPlaylistGuardApi/routes/getAuth.ts
@@ -3,12 +3,13 @@ import { InvalidResponseDataError } from '../../../errors';
 export async function getAuth(code: string) {
     const apiUrl = process.env.NEXT_PUBLIC_API_URL || '';
     const response = await fetch(`${apiUrl}/auth/redirect?code=${code}`);
+    const { status } = response;
 
-    if (response.status !== 201) throw new Error('Invalid response');
+    if (status !== 201) return { status, data: null };
 
     const token = response.headers.get('Authorization')?.split(' ')[1];
 
     if (!token) throw new InvalidResponseDataError('Invalid token received');
 
-    return { token };
+    return { status, data: token };
 }
--- src/useCases/auth.ts
+++ src/useCases/auth.ts
@@ -2,6 +2,7 @@ import { GetServerSidePropsContext } from 'next';
 import { CookieKey } from '../@types';
 import { parseCookies, setCookie } from 'nookies';
 import { getAuth } from '../services/spotifyPlaylistGuardApi';
+import { InternalServerError } from '../errors';
 
 export async function authenticate(
     code: string,
@@ -9,7 +10,11 @@ export async function authenticate(
 ) {
     const tokenCookieKey: CookieKey = 's-p-guard:token';
 
-    const { token } = await getAuth(code);
+    const token = await getAuth(code).then(({ status, data }) => {
+        if (status !== 201 || !data) throw new InternalServerError({});
+
+        return data;
+    });
 
     setCookie(context, tokenCookieKey, token, {
         maxAge: 60 * 60 * 1,
