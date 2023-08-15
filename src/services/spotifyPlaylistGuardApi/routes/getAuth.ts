79f470e35e14887213c5b0b3e9a5cd5a64c88b91
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-08-14 23:35:32 -0300

    fix: change res body from getAuth service

--- src/services/spotifyPlaylistGuardApi/routes/getAuth.ts
+++ src/services/spotifyPlaylistGuardApi/routes/getAuth.ts
@@ -1,6 +1,23 @@
+import { z } from 'zod';
 import { SpotifyPlaylistGuardApiReturn } from '../.';
 import { InvalidResponseDataError } from '../../../errors';
 import { request } from '../httpClient';
+
+const authSchema = z.object({
+    token: z.string(),
+});
+
+function validateAuthSchema(payload: unknown) {
+    const validation = authSchema.safeParse(payload);
+    const { success } = validation;
+
+    if (!success)
+        throw new InvalidResponseDataError(
+            JSON.stringify(validation.error.issues),
+        );
+
+    return validation.data;
+}
 
 export async function getAuth(
     code: string,
@@ -9,15 +26,12 @@ export async function getAuth(
         path: `/auth/redirect?code=${code}`,
         authenticated: false,
     });
-
     const { status } = response;
+    const resBody = await response.json().catch(() => ({}));
 
     if (status !== 201) return { success: false, status, data: null };
 
-    const token = response.headers.get('Authorization')?.split(' ')[1];
-
-    if (!token || typeof token !== 'string')
-        throw new InvalidResponseDataError('Invalid token received');
+    const { token } = validateAuthSchema(resBody);
 
     return { success: true, status, data: token };
 }
