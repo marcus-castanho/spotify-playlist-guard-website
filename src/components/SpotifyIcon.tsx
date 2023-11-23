f0f7f9dad68baaeb9db2ce50e562f22f2f38b9f5
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-23 16:53:22 -0300

    feat: remove unused components

--- src/components/SpotifyIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-import SpotifyLogo from '../../public/assets/spotify_icon_RGB_black.svg';
-import SvgIcon from '@mui/material/SvgIcon';
-
-const SpotifyIcon: FC = () => {
-    return <SvgIcon component={SpotifyLogo} inheritViewBox />;
-};
-
-export default SpotifyIcon;
