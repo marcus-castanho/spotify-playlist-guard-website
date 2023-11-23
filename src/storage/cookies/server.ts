e2019e0363ae6329a14559f89065aa060fb2badc
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-22 23:16:32 -0300

    feat: create storage/cookies/server

---
+++ src/storage/cookies/server.ts
@@ -0,0 +1,152 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { CookieKey } from '.';
+import { GetServerSidePropsContext } from 'next';
+
+/**
+ * Get a cookie with a particular name on the server side of the app. This function can be used in:
+ * - getServerSideProps function
+ *
+ * @param key Cookie key.
+ */
+export function getPageReqCookie(
+    key: CookieKey,
+    req: GetServerSidePropsContext['req'],
+) {
+    return req.cookies[key];
+}
+
+/**
+ * Get cookies on the server side of the app. This function can be used in:
+ * - getServerSideProps function
+ */
+export function getPageReqCookies(req: GetServerSidePropsContext['req']) {
+    return req.cookies;
+}
+
+/**
+ * Get a cookie with a particular name on the server side of the app. This function can be used with a Request instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param key Cookie key.
+ * @param req Request instance.
+ */
+export function getRequestCookie(key: CookieKey, req: NextRequest) {
+    return req.cookies.get(key)?.value;
+}
+
+/**
+ * Get cookies on the server side of the app. This function can be used with a Request instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param req Request instance.
+ */
+export function getRequestCookies(req: NextRequest) {
+    const cookiesArray = req.cookies.getAll();
+
+    return cookiesArray.reduce(
+        (cookiesObj, { name, value }) =>
+            Object.assign(cookiesObj, { [name]: value }),
+        {} as {
+            [key: string]: string;
+        },
+    );
+}
+
+/**
+ * Get a cookie with a particular name on the server side of the app. This function can be used with a Response instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param key Cookie key.
+ * @param res Response instance.
+ */
+export function getResponseCookie(key: CookieKey, res: NextResponse) {
+    return res.cookies.get(key)?.value;
+}
+
+/**
+ * Get cookies on the server side of the app. This function can be used with a Response instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param res Response instance.
+ */
+export function getResponseCookies(res: NextResponse) {
+    const cookiesArray = res.cookies.getAll();
+
+    return cookiesArray.reduce(
+        (cookiesObj, { name, value }) =>
+            Object.assign(cookiesObj, { [name]: value }),
+        {} as {
+            [key: string]: string;
+        },
+    );
+}
+
+/**
+ * Set a cookie with a particular name on the server side of the app. This function can be used with a Request instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param key Cookie key.
+ * @param value Cookie value.
+ * @param req Request instance.
+ */
+export function setRequestCookie(
+    key: CookieKey,
+    value: string,
+    req: NextRequest,
+) {
+    req.cookies.set({ name: key, value });
+}
+
+/**
+ * Set a cookie with a particular name on the server side of the app. This function can be used with a Response instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param key Cookie key.
+ * @param value Cookie value.
+ * @param res Response instance.
+ * @param options Options that are passed down to `cookie` library.
+ */
+export function setResponseCookie(
+    key: CookieKey,
+    value: string,
+    res: NextResponse,
+    options?: Omit<
+        Exclude<Parameters<NextResponse['cookies']['set']>[0], string>,
+        'name' | 'value'
+    >,
+) {
+    res.cookies.set({ name: key, value, ...options });
+}
+
+/**
+ * Delete cookies with particular names on the server side of the app. This function can be used with a Request instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param keys Cookies keys.
+ * @param req Request instance.
+ */
+export function deleteRequestCookies(
+    cookieKeys: CookieKey[],
+    req: NextRequest,
+) {
+    req.cookies.delete(cookieKeys);
+}
+
+/**
+ * Delete a cookie with a particular name on the server side of the app. This function can be used with a Response instance in:
+ * - Next middleware
+ * - Next route handlers
+ *
+ * @param key Cookie key.
+ * @param res Response instance.
+ */
+export function deleteResponseCookie(cookieKey: CookieKey, res: NextResponse) {
+    res.cookies.delete(cookieKey);
+}
