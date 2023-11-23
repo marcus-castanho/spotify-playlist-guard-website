41da1b8ad91f563e93ccf4373156737f9992c448
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-22 23:29:16 -0300

    feat: create config for public and private pages

---
+++ src/config/pages.ts
@@ -0,0 +1,18 @@
+export const publicPages = ['/', '/signin', '/500', '400'] as const;
+export type PublicPage = typeof publicPages[number];
+
+export const privatePages = ['/home', '/profile', '/playlist'] as const;
+export type PrivatePage = typeof privatePages[number];
+
+export const isPublicPage = (path: string) => {
+    return publicPages.some((page) => {
+        if (page === '/') return path === page;
+        return path.startsWith(page);
+    });
+};
+
+export const isPrivatePage = (path: string) => {
+    return privatePages.some((page) => {
+        return path.startsWith(page);
+    });
+};
