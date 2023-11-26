4a59328da7dcde8e71ced9f3b7cd835b6e1a6908
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-26 18:51:09 -0300

    refactor: move authenticate to signin as a function

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
-import { GetServerSideProps, NextPage } from 'next';
-import { authenticate } from '@/useCases/authenticate';
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next';
 import { log } from '../logger';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { SignIn as SignInPage } from '@/views/SignIn';
+import { getAuth } from '@/services/spotifyPlaylistGuardApi';
+import { InternalServerError } from '@/errors';
+import { setPageResCookies } from '@/storage/cookies/server';
+import { TOKEN_COOKIE_KEY } from '@/contexts/AuthContext';
+
+async function authenticate(code: string, context: GetServerSidePropsContext) {
+    const token = await getAuth(code).then(({ success, data }) => {
+        if (!success) throw new InternalServerError({});
+        return data;
+    });
+
+    setPageResCookies(context, TOKEN_COOKIE_KEY, token, {
+        maxAge: 60 * 60 * 1,
+    });
+}
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const locale = context.locale || '';
