47c0a9e3145dbe85018c6d8578a2bdcd0f58b7d9
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-27 20:27:22 -0300

    chore: remove @svg/webpack dependecy

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const { i18n } = require('./next-i18next.config');
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.svg$/i,
-            issuer: /\.[jt]sx?$/,
-            use: ['@svgr/webpack'],
-        });
-
-        return config;
-    },
     i18n,
     async rewrites() {
         return [
