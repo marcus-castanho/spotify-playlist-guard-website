bbf67786b1b73f257586bc1f282dad38b1680083
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-28 23:20:08 -0300

    feat: add workaround in _document to set theme in html on server

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,65 @@
 import { THEME_COOKIE_KEY } from '@/contexts/ThemeContext';
-import { getCookie } from '@/storage/cookies/client';
-import { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+    DocumentInitialProps,
+} from 'next/document';
 import { match } from 'ts-pattern';
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body className={`dark:bg-black dark:text-white`}>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+class MyDocument extends Document {
+    static initialTheme = 'light';
+
+    static getCookie(ctx: DocumentContext, key: string) {
+        const cookiesStr = ctx.req?.headers.cookie || '';
+        const cookiesArr = cookiesStr.split(';');
+        const cookieStr =
+            cookiesArr.find((cookie) => {
+                const cookieKey = cookie.split('=')[0];
+                return cookieKey === key;
+            }) || '';
+        const value = cookieStr.split('=')[1];
+
+        return value;
+    }
+
+    static async getInitialProps(
+        ctx: DocumentContext,
+    ): Promise<DocumentInitialProps> {
+        const originalRenderPage = ctx.renderPage;
+        const initialTheme =
+            MyDocument.getCookie(ctx, THEME_COOKIE_KEY) || 'light';
+        MyDocument.initialTheme = initialTheme;
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                enhanceApp: (App) => App,
+                enhanceComponent: (Component) => Component,
+            });
+
+        const initialProps = await Document.getInitialProps(ctx);
+
+        return initialProps;
+    }
+
+    render() {
+        return (
+            <Html
+                lang="en"
+                className={match(MyDocument.initialTheme)
+                    .with('light', () => '')
+                    .otherwise(() => MyDocument.initialTheme)}
+            >
+                <Head />
+                <body className={`dark:bg-black dark:text-white`}>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
+
+export default MyDocument;
