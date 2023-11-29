b947aaf7a4a2c8e7af89c1d22447ff96dd0422f9
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-11-29 13:15:58 -0300

    chore: add config to cypress

---
+++ cypress/support/command.ts
@@ -0,0 +1,8 @@
+// ***********************************************
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+/// <reference types="cypress" />
+
+import '@testing-library/cypress/add-commands';
