e9dcaa6a028bd37af747b1c4478b909fcc82f36c
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-09 21:08:05 -0300

    feat: create GuardIcon and stories

--- src/stories/pages/Foundations/Icons/icons.tsx
+++ src/stories/pages/Foundations/Icons/icons.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { MoonIcon } from '@/components/icons/MoonIcon';
 import { SunIcon } from '@/components/icons/SunIcon';
+import { GuardIcon } from '@/components/icons/GuardIcon';
 
 export const icons: { title: string; component: ReactNode }[] = [
     {
@@ -10,5 +11,9 @@ export const icons: { title: string; component: ReactNode }[] = [
     {
         title: 'sun',
         component: <SunIcon size={24} key={MoonIcon.name} />,
+    },
+    {
+        title: 'guard',
+        component: <GuardIcon size={24} key={GuardIcon.name} />,
     },
 ];
