4fe904447d0d1129bb1455a1d2b83c9b772c389a
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-18 23:02:19 -0300

    feat: style InternalServerError page

--- src/views/InternalServerError.tsx
+++ src/views/InternalServerError.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { PageContainer } from '@/components/PageContainer';
+import { Main } from '@/components/Main';
+import { ServerWarningIcon } from '@/components/icons/ServerWarningIcon';
+import { Anchor } from '@/components/Anchor';
+import { useTheme } from '@/contexts/ThemeContext';
 
 export const InternalServerError = () => {
-    return <h1>500 - Server-side error occurred</h1>;
+    const { theme } = useTheme();
+
+    return (
+        <PageContainer>
+            <Main>
+                <div className="flex w-full items-center justify-center">
+                    <div className="flex flex-col items-center">
+                        <ServerWarningIcon
+                            size={60}
+                            fillColor={theme === 'dark' ? 'white' : 'black'}
+                        />
+                        <h1 className="flex justify-center px-6 pb-4 pt-10 text-center text-5xl font-bold sm:text-left">
+                            500 - Internal server error
+                        </h1>
+                        <p className="px-6 pb-10 text-gray-100">
+                            Sorry, something went wrong
+                        </p>
+                        <Anchor
+                            text="Go Home"
+                            href="/"
+                            target="_self"
+                            label="home-link"
+                        />
+                    </div>
+                </div>
+            </Main>
+        </PageContainer>
+    );
 };
