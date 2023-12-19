aa8acd4dfe312042dad4bf94b54343141543c5f1
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-12-18 23:03:34 -0300

    feat: style NotFound page

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { PageContainer } from '@/components/PageContainer';
+import { Main } from '@/components/Main';
+import { Anchor } from '@/components/Anchor';
+import { GuardBotLogo } from '@/components/GuardBotLogo';
 
 export const NotFound = () => {
-    return <h1>404 - Page Not Found</h1>;
+    return (
+        <PageContainer>
+            <Main>
+                <div className="flex w-full items-center justify-center">
+                    <div className="flex flex-col items-center">
+                        <GuardBotLogo size={60} />
+                        <h1 className="px-6 pb-4 pt-10 text-center text-5xl font-bold sm:text-left">
+                            Page not found
+                        </h1>
+                        <p className="px-6 pb-10 text-center text-gray-100 sm:text-left">
+                            The resource you are looking for could not be found
+                        </p>
+                        <Anchor
+                            text="Go Home"
+                            href="/"
+                            target="_self"
+                            label="home-link"
+                        />
+                    </div>
+                </div>
+            </Main>
+        </PageContainer>
+    );
 };
