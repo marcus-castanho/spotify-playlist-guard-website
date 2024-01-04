008bd106d5f68210666ac06c05d689c8aa907edb
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-03 22:11:52 -0300

    feat: add hover background color in PlaylistCard

--- src/views/Home/components/PlaylistCard.tsx
+++ src/views/Home/components/PlaylistCard.tsx
@@ -36,7 +36,7 @@ export const PlaylistCard: FC<PlaylistCardProps> = ({
     const { toast } = useToast();
 
     return (
-        <div className="rounded-lg border-2 bg-white  p-1 shadow-md max-sm:h-full sm:w-[200px] dark:border-0 dark:bg-gray-950 dark:hover:bg-gray-800">
+        <div className="rounded-lg border-2 bg-white p-1 shadow-md hover:bg-gray-50 max-sm:h-full sm:w-[200px] dark:border-0 dark:bg-gray-950 dark:hover:bg-gray-800">
             <div className="flex h-full flex-col justify-between">
                 <div className="flex p-4 sm:flex-col">
                     <div className="relative h-40 w-40 min-w-[10rem]">
