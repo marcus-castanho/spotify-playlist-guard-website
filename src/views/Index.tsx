22990eb5db9d455028909ab04ca9811f3df0f2bd
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-04 21:59:54 -0300

    feat: use Main and Footer in index page

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 import { PageContainer } from '@/components/PageContainer';
 import { Header } from '@/components/Header';
+import { Main } from '@/components/Main';
+import { Footer } from '@/components/Footer';
 
 export const Index = () => {
     return (
         <PageContainer>
             <Header />
-            <Link href="/signin">Sign in</Link>
+            <Main>
+                <div className="flex w-full items-center justify-center">
+                    <Link href="/signin">Sign in</Link>
+                </div>
+            </Main>
+            <Footer />
         </PageContainer>
     );
 };
