64fd8136d8260d96078c5349f3d52551eb4c19a0
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-05 23:01:03 -0300

    feat: remove size prop from DefaultAlbumCover
    -change component
    -update tests

--- src/components/DefaultAlbumCover.tsx
+++ src/components/DefaultAlbumCover.tsx
@@ -1,20 +1,14 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { MusicalNoteIcon } from './icons/MusicalNoteIcon';
 import { colors } from '@/styles/theme';
 
-type DefaultAlbumCoverProps = {
-    size?: number;
-};
-
-export const DefaultAlbumCover: FC<DefaultAlbumCoverProps> = ({
-    size = 140,
-}) => {
+export const DefaultAlbumCover = () => {
     return (
         <div
             aria-label="default-album-cover"
-            className="inline-block border-2 border-black bg-gray-400 p-2"
+            className="inline-block w-full border-2 border-black bg-gray-400 p-2"
         >
-            <MusicalNoteIcon size={size} fillColor={colors.gray['100']} />
+            <MusicalNoteIcon fillColor={colors.gray['100']} />
         </div>
     );
 };
--- src/tests/DefaultAlbumCover.spec.tsx
+++ src/tests/DefaultAlbumCover.spec.tsx
@@ -3,12 +3,10 @@ import { render } from '@testing-library/react';
 import { DefaultAlbumCover } from '@/components/DefaultAlbumCover';
 
 const LABEL = 'default-album-cover';
-
 const LOGO_ICON_TITLE = 'musical-note-icon';
 
 describe('DefaultAlbumCover', () => {
     it('should render', () => {
-        const DEFAULT_SIZE = 140;
         const { getByLabelText, getByTitle } = render(<DefaultAlbumCover />);
 
         const container = getByLabelText(LABEL);
@@ -16,18 +14,6 @@ describe('DefaultAlbumCover', () => {
         const svg = svgIconTitle.parentElement;
 
         expect(container).toBeDefined();
-        expect(svg?.getAttribute('width')).toBe(`${DEFAULT_SIZE}`);
-        expect(svg?.getAttribute('height')).toBe(`${DEFAULT_SIZE}`);
-    });
-
-    it('should render with given size', () => {
-        const SIZE = 60;
-        const { getByTitle } = render(<DefaultAlbumCover size={SIZE} />);
-
-        const svgIconTitle = getByTitle(LOGO_ICON_TITLE);
-        const svg = svgIconTitle.parentElement;
-
-        expect(svg?.getAttribute('width')).toBe(`${SIZE}`);
-        expect(svg?.getAttribute('height')).toBe(`${SIZE}`);
+        expect(svg).toBeDefined();
     });
 });
