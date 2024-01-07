e53818c8be038a4554f039664062abb836d43874
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-07 00:41:28 -0300

    feat: use new Hero in Index

--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import Link from 'next/link';
 import { PageContainer } from '@/components/PageContainer';
 import { Header } from '@/components/Header';
 import { Main } from '@/components/Main';
 import { Footer } from '@/components/Footer';
+import { Hero } from './components/Hero';
 
 export const Index = () => {
     return (
         <PageContainer>
             <Header />
             <Main>
-                <div className="flex w-full items-center justify-center">
-                    <Link href="/signin">Sign in</Link>
+                <div className="w-full p-6">
+                    <Hero />
+                    <Footer />
                 </div>
             </Main>
-            <Footer />
         </PageContainer>
     );
 };
