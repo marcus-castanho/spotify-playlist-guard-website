464adfbd00749269f36282224ef26e10eb5d30cc
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-07 09:52:08 -0300

    feat: style Hero for mobile screen size

--- src/views/Index/components/Hero.tsx
+++ src/views/Index/components/Hero.tsx
@@ -4,9 +4,9 @@ import { RedirectToSignInButton } from './RedirectToSignInButton';
 
 export const Hero = () => {
     return (
-        <div className="flex h-full rounded-lg dark:bg-gray-950">
-            <div className="flex h-full w-[50%] flex-col items-center justify-center gap-8 p-16">
-                <div className="flex flex-col gap-4">
+        <div className="flex h-full rounded-lg max-sm:flex-col dark:bg-gray-950">
+            <div className="flex h-full flex-col items-center justify-center gap-8 p-4 sm:w-[50%] sm:p-16">
+                <div className="flex flex-col gap-4 text-center">
                     <h1 className="text-5xl font-semibold">
                         A bot to guard your collaborative playlists
                     </h1>
@@ -18,7 +18,7 @@ export const Hero = () => {
                 </div>
                 <RedirectToSignInButton />
             </div>
-            <div className="flex h-full w-[50%] flex-col items-center justify-center gap-8 p-16">
+            <div className="flex h-full flex-col items-center justify-center gap-8 p-4 sm:w-[50%] sm:p-16">
                 <div className="relative rounded-md bg-black p-3">
                     <Image
                         alt="playlist-spotify-display"
@@ -28,17 +28,22 @@ export const Hero = () => {
                         className="rounded-lg"
                         unoptimized
                     />
-                    <div className="absolute bottom-0 left-0 h-[105px] w-[105px] translate-x-[-50%] translate-y-[50%] ">
-                        <div className="h-full w-full animate-spin rounded-[50%] bg-gradient-to-b from-primary-verdant to-black" />
+                    <div className="absolute bottom-0 left-[50%] translate-x-[-50%] translate-y-[50%] sm:left-0">
+                        <div className="relative h-[80px] w-[80px] sm:h-[105px] sm:w-[105px]">
+                            <div className="absolute bottom-[50%] left-[50%] h-[105%] w-[105%] translate-x-[-50%] translate-y-[50%]">
+                                <div className="h-full w-full animate-spin rounded-[50%] bg-gradient-to-b from-primary-verdant to-black" />
+                            </div>
+                            <div className="absolute bottom-[50%] left-[50%] h-full w-full translate-x-[-50%] translate-y-[50%]">
+                                <Image
+                                    alt="playlist-spotify-display"
+                                    src="/assets/images/guardBot-191414-circle.png"
+                                    fill
+                                    className="rounded-[50%]"
+                                    unoptimized
+                                />
+                            </div>
+                        </div>
                     </div>
-                    <Image
-                        alt="playlist-spotify-display"
-                        src="/assets/images/guardBot-191414-circle.png"
-                        width={100}
-                        height={0}
-                        className="absolute bottom-0 left-0 translate-x-[-50%] translate-y-[50%] rounded-[50%]"
-                        unoptimized
-                    />
                 </div>
             </div>
         </div>
