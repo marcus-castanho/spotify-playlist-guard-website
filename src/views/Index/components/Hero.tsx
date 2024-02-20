9ad2e46c5e9f7051578fb3eab57f784448ce2ec1
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-02-19 21:17:34 -0300

    feat: change Hero - remove screen image

--- src/views/Index/components/Hero.tsx
+++ src/views/Index/components/Hero.tsx
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { RedirectToSignInButton } from './RedirectToSignInButton';
+import { ImageLoader } from '@/components/ImageLoader';
+
+const GuardBotSign = () => {
+    const [loaded, setLoaded] = useState(false);
+
+    return (
+        <div className="relative h-60 w-60 sm:h-72 sm:w-72">
+            <div className="absolute bottom-[50%] left-[50%] h-[102.5%] w-[102.5%] translate-x-[-50%] translate-y-[50%]">
+                <div className="h-full w-full animate-spin rounded-[50%] bg-gradient-to-b from-primary-verdant to-black" />
+            </div>
+            <div className="absolute bottom-[50%] left-[50%] h-full w-full translate-x-[-50%] translate-y-[50%]">
+                <ImageLoader
+                    loaded={loaded}
+                    bgImageSrc="assets/images/guardBot-191414-circle-resized_10x10.png"
+                >
+                    <Image
+                        alt="playlist-spotify-display"
+                        src="assets/images/guardBot-191414-circle.png"
+                        fill
+                        className={'rounded-[50%]'}
+                        unoptimized
+                        onLoad={() => setLoaded(true)}
+                    />
+                </ImageLoader>
+            </div>
+        </div>
+    );
+};
 
 export const Hero = () => {
     return (
-        <div className="flex h-full rounded-lg max-sm:flex-col dark:bg-gray-950">
+        <div className="flex h-full rounded-lg max-sm:flex-col-reverse dark:bg-black">
             <div className="flex h-full flex-col items-center justify-center gap-8 p-4 sm:w-[50%] sm:p-16">
                 <div className="flex flex-col gap-4 text-center">
-                    <h1 className="text-5xl font-semibold">
+                    <h1 className="text-4xl font-semibold sm:text-5xl">
                         A bot to guard your collaborative playlists
                     </h1>
                     <p className="text-lg">
@@ -19,32 +47,7 @@ export const Hero = () => {
                 <RedirectToSignInButton />
             </div>
             <div className="flex h-full flex-col items-center justify-center gap-8 p-4 sm:w-[50%] sm:p-16">
-                <div className="relative rounded-md bg-black p-3">
-                    <Image
-                        alt="playlist-spotify-display"
-                        src="/assets/images/playlist-spotify-display.png"
-                        width={600}
-                        height={0}
-                        className="rounded-lg"
-                        unoptimized
-                    />
-                    <div className="absolute bottom-0 left-[50%] translate-x-[-50%] translate-y-[50%] sm:left-0">
-                        <div className="relative h-[80px] w-[80px] sm:h-[105px] sm:w-[105px]">
-                            <div className="absolute bottom-[50%] left-[50%] h-[105%] w-[105%] translate-x-[-50%] translate-y-[50%]">
-                                <div className="h-full w-full animate-spin rounded-[50%] bg-gradient-to-b from-primary-verdant to-black" />
-                            </div>
-                            <div className="absolute bottom-[50%] left-[50%] h-full w-full translate-x-[-50%] translate-y-[50%]">
-                                <Image
-                                    alt="playlist-spotify-display"
-                                    src="/assets/images/guardBot-191414-circle.png"
-                                    fill
-                                    className="rounded-[50%]"
-                                    unoptimized
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <GuardBotSign />
             </div>
         </div>
     );
